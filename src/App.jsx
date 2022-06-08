b203f2999c51ba4509c2b4591586218c5c9d0699
Katalin Kovacs <kovacs.katalin89@gmail.com>
2022-06-08 15:37:02 +0200

    minor changes in the component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Navigation />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Navigation from "../Navigation/Navigation";
+
+function Header() {
+  return (
+    <>
+      <h1>ReactExperiment</h1>
+      <Navigation />
+    </>
+  );
+}
+
+export default Header;
